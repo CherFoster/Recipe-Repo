d5c5b1f21cbf7de07e4c883d66f8e197b044bcc4
Cherise Foster <cherise.foster@outlook.com>
2024-01-31 13:17:22 -0700

    started models.py

--- server/models.py
+++ server/models.py
@@ -1,6 +1,47 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
 
-from config import db
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
 
-# Models go here!
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    # Takes password, hashes it, and stores the hash
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    # compares hashed password to the stored hash  
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+class Recipe(db.Model, SerializerMixin):
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    instructions = db.Column(db.Text)
+
+    def __repr__(self):
+        return (
+            f'Name: {self.name}, ' \
+            + f'Image: {self.image},' \
+            + f'Ingredients: {self.ingredients},' \
+            + f'Instructions: {self.instructions},' \
+        )
